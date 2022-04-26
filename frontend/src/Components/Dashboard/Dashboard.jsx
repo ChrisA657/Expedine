1ddc40296f4bdd31dd67a73cf8d239089b2cc7e0
Elliot Boesch <elliot.boesch@gmail.com>
2022-04-26 17:44:36 -0500

    Removed orders and changed fliter options

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -17,9 +17,7 @@ const Dashboard = () => {
     const [farmId, setfarmId] = useState(null);
     const [showCreateFarm, setShowCreateFarm] = useState(false);
     const [orders, setOrders] = useState([
-        new order(1, 1, "Zach", "Greenville Farms", 2, new Date(), 242, [new item("Horse", "Big horse", "https://media.gq.com/photos/56e71c0b14cbe0637b261d7f/16:9/w_2560%2Cc_limit/horseinsuit2.jpg", 20, 1, 2), new item("Broom", "Dust destroyer", "https://images.thdstatic.com/productImages/6aa74e47-b7ba-48c8-bea8-258298b0ee8e/svn/hdx-corn-brooms-750-64_600.jpg", 20, 2, 2)],"Dan", "The man","323 really real street","Dallas","Texas",75115, false),
-        new order(1, 1, "Texas", "Greenville Farms", 2, new Date(), 242, [new item("Horse", "Big horse", "https://media.gq.com/photos/56e71c0b14cbe0637b261d7f/16:9/w_2560%2Cc_limit/horseinsuit2.jpg", 20, 1, 2)], true),
-        new order(1, 1, "Mom", "Greenville Farms", 2, new Date(), 242, [new item("Horse", "Big horse", "horse.jpg", 20, 1, 2)], false),
+
     ])
 
     const eventContext = useContext(EventContext);
